3f1c6d50966fde25c26c2af60f54748fb74d7806
Daehyun Kim <kimdhyungg@gmail.com>
2024-12-15 03:19:11 +0900

    move to examples

--- examples/hello-redis.rs
+++ examples/hello-redis.rs
@@ -3,11 +3,15 @@ use mini_redis::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    println!("Hello, world!");
     let mut client = client::connect("127.0.0.1:6379").await?;
+    println!("Hello, world!2");
 
     client.set("hello", "world".into()).await?;
+    println!("Hello, world!3");
 
     let result = client.get("hello").await?;
+    println!("Hello, world!4");
 
     println!("got value from the server; result={:?}", result);
 
