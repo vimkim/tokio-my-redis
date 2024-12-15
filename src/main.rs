44e95a22c36c7d88da01f7ddbcbf810b6f8ffb85
Daehyun Kim <kimdhyungg@gmail.com>
2024-12-15 19:34:25 +0900

    feat(main): implement GET and SET in process

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn main() {
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn old_process(socket: TcpStream) {
     // The `Connection` lets us read/write redis **frames** instead of
     // byte streams. The `Connection` type is defined by mini-redis.
     let mut connection = Connection::new(socket);
@@ -29,5 +29,33 @@ async fn process(socket: TcpStream) {
         // Respond with an error
         let response = Frame::Error("unimplemented".to_string());
         connection.write_frame(&response).await.unwrap();
+    }
+}
+
+async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    let mut db = HashMap::new();
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                println!("set {:?} {:?}", cmd.key(), cmd.value());
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                println!("get {:?}", cmd.key());
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+        connection.write_frame(&response).await.unwrap()
     }
 }
