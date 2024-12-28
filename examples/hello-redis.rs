c7fa62e01b578a14b0b1db96682378c4b88cd448
Daehyun Kim <kimdhyungg@gmail.com>
2024-12-28 23:39:40 +0900

    test tracing crate

--- examples/hello-redis.rs
+++ examples/hello-redis.rs
@@ -1,8 +1,29 @@
 use mini_redis::client;
 use mini_redis::Result;
+use tracing::info;
+use tracing::instrument;
+
+#[instrument]
+fn my_function(arg: i32) {
+    info!("Inside my_function");
+    another_function();
+}
+
+#[instrument]
+fn another_function() {
+    info!("Inside another_function");
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .with_line_number(true)
+        .with_target(true)
+        .init();
+
+    my_function(5);
+
     println!("Hello, world!");
     let mut client = client::connect("127.0.0.1:6379").await?;
     println!("Hello, world!2");
@@ -10,6 +31,7 @@ async fn main() -> Result<()> {
     client.set("hello", "world".into()).await?;
     println!("Hello, world!3");
 
+    my_function(5);
     let result = client.get("hello").await?;
     println!("Hello, world!4");
 
