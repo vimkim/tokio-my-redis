49f13394d2d0931ee3d285cd019aa2800b52a206
Daehyun Kim <kimdhyungg@gmail.com>
2024-12-28 23:39:16 +0900

    remove errors

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,18 +1,10 @@
 use mini_redis::client;
 use tokio::sync::mpsc;
 
-use bytes::Bytes;
-
-#[derive(Debug)]
-enum Command {
-    Get { key: String },
-    Set { key: String, val: Bytes },
-}
-
 #[tokio::main]
 async fn main() {
     color_backtrace::install();
-    let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+    client::connect("127.0.0.1:6379").await.unwrap();
 
     let (tx, mut rx) = mpsc::channel(32);
     let tx2 = tx.clone();
